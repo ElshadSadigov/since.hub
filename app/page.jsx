952133f7fb23ab92bbb58544bf69fd5ca05bb9d1
Elshad Sadigov <sv.elshad@gmail.com>
2024-03-19 22:48:59 +0400

    home page codeing

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,72 @@
+import AboutCard from "@/components/about-card";
+import aboutIcon1 from '../public/svgs/about-icon1.svg'
+import aboutIcon2 from '../public/svgs/about-icon2.svg'
+import aboutIcon3 from '../public/svgs/about-icon3.svg'
+import aboutImg from '../public/imgs/about-img.png'
+import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div>
-      Home page
+      <div>
+        Slide part
+      </div>
+      <div className="w-full flex items-center justify-between ps-[80px] bg-[#EEFAFA]">
+        <div className="flex flex-col items-start">
+          <span className="text-[36px] text-[#050123] font-medium">Haqqımızda</span>
+          <span className="mt-[10px] text-[#050123] text-opacity-50 text-[20px]  font-medium">3 əsas mərhələdə haqqımızda daha çox məlumat əldə edin</span>
+          <div className="mt-[50px] flex gap-[32px] ">
+            <AboutCard 
+            img={aboutIcon1} 
+            color = 'orange-400'
+            title='Dəyərlərimiz'
+            text='İşimizi sadiqliklə, şəffaflıqla və etik normativlərlə üstün tuturuq. Müştərilərimiz və tərəfdaşlarımızla sağlam əlaqələr qurmağa önəlik veririk.'
+            />
+            <AboutCard 
+            img={aboutIcon2}
+            color ='red-400'
+            title=' Hədəflərimiz'
+            text='Mükəmməl xidmət təklifi üçün planlı və yüksək standartlarda çalışmağı hədəfləyirik. Uzunmüddətli nailiyyət əldə etməyi hədəfləyirik.'
+            />
+            <AboutCard
+            img={aboutIcon3}
+            color='green-400'
+            title='Missiyamız'
+            text='Müştərilərimizə uğur yolculuğunda tərəfdaşlıq və dəstək təmin etmək istəyirik. Layiqli xidmət missiyamızın əsasını təşkil edir.'
+            />
+          </div>
+        </div>
+        <div className="bg-gradient-to-br from-transparent via-teal-100 to-teal-300">
+          <Image src={aboutImg} alt="about img" className="w-[350px] h-full"/>
+        </div>
+      </div>
+      <div className="flex gap-[24px] px-[80px] pt-[120px] pb-[150px]">
+        <div className="bg-[#374AF3] rounded-[16px] flex flex-col w-1/4">
+          <span>Bloglar və daha çoxu</span>
+          <span>#Kitablar</span>
+          <li>Məqalələr</li>
+        </div>
+        <div className="px-[28px] py-[25px] flex flex-col items-start w-3/4">
+          <span className="text-[16px] text-[#374af3] font-medium">İndi başla</span>
+          <span className="text-[28px] text-[#050123] font-medium">Paylaşmaq üçün buradasınız: Bilik İcmasına xoş gəlmisiniz!</span>
+          <ul className="ms-[15px] mt-[20px] text-[#050123] text-opacity-50 list-disc flex flex-wrap gap-x-[55px] gap-y-[15px]">
+            <li>Universitet məlumatları</li>
+            <li>Araşdırma-tədqiqat</li>
+            <li>Faydalı məlumatlar</li>
+            <li>Fayllar</li>
+            <li>Yazılar</li>
+            <li>Kitablar</li>
+            <li>Bloglar</li>
+            <li>Məqalələr</li>
+            <li>Disertasiyalar</li>
+            <li>Yenilik-xəbər</li>
+            <li>Sual cavab</li>
+          </ul>
+          <Link href='/' className="px-[32px] py-[16px] mt-[50px] bg-[#374af3] rounded-[8px] text-[16px] font-bold text-[#fff]">Paylaşım et</Link>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/header.jsx
@@ -0,0 +1,59 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import instagram from '../public/svgs/instagram.svg'
+import facebook from '../public/svgs/facebook.svg'
+import mail from '../public/svgs/mail.svg'
+import search from '../public/svgs/search.svg'
+import down from '../public/svgs/down.svg'
+
+const Header = () => {
+  return (
+    <div className='w-full fixed left-0 top-0'>
+      <div className='flex justify-between px-[80px] py-[20px] bg-[#fff]'>
+        <div className='flex items-center gap-[20px]'>
+            <Link href='/' className='flex items-center gap-[10px]'>
+                <Image src={instagram} alt='instagram'/>
+                <span className='text-[14px] text-[#050123]'>Instagram</span>
+            </Link>
+            <div className='w-[2px] h-[18px] rounded-[15px] bg-[#000]'></div>
+            <Link href='/' className='flex items-center gap-[10px] '>
+                <Image src={facebook} alt='facebook'/>
+                <span className='text-[14px] text-[#050123]'>Facebook</span>
+            </Link>
+        </div>
+        <div>
+            <Link href='/' className='flex items-center gap-[10px]'>
+                <Image src={mail} alt='mail'/>
+                <span className='text-[14px] text-[#050123]'>infosince.hub@gmail.com</span>
+            </Link>
+        </div>
+      </div>
+      <div className='px-[80px] py-[20px] flex justify-between items-center w-full bg-[#374AF3]'>
+        <div className='flex items-start'>
+            <span className='text-[36px] font-bold text-[#050123]'>SİNCE</span>
+            <span className='text-[36px] font-bold text-[#fff]'>.HUB</span>
+            <span className='font-black text-[#fff] text-[36px] translate-y-[-25px]'>.</span>
+        </div>
+        <div className='flex items-center gap-[180px]'>
+            <ul className='flex items-center gap-[40px] text-[16px] text-[#fff] font-medium'>
+                <li><Link href='/'>Haqqımızda</Link></li>
+                <li><Link href='/'>Qaydalar</Link></li>
+                <li><Link href='/'>Xidmətlər</Link></li>
+                <li><Link href='/'>Əlaqə</Link></li>
+            </ul>
+            <div className='flex items-center gap-[30px]'>
+                <Link href='/'><Image src={search} alt='search'/></Link>
+                <div className='flex items-center gap-[7px] cursor-pointer'> 
+                    <span className='text-[#fff] text-[24px] '>AZ</span>
+                    <Image src={down} alt='sown'/>
+                </div>
+                <Link href='/' className='px-[21px] py-[10px] border-[1px] border-[#fff] rounded-[8px] text-[16px] font-medium text-[#fff]'>Daxil ol</Link>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
