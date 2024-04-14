3e255534e5fe333516baeca3a772e34a39c311a9
Ramin404 <rsadiqov1@std.beu.edu.az>
2024-04-14 14:01:39 +0400

    responsive design

--- app/register/sign-in/page.jsx
+++ app/register/sign-in/page.jsx
@@ -6,16 +6,16 @@ import googleIcon from '../../../public/svgs/google-icon.svg'
 
 const page = () => {
     return (
-        <div className='flex flex-col items-start px-[80px] py-[80px] '>
+        <div className='flex flex-col items-start px-[30px] md:px-[80px] pt-[40px] md:pt-[80px] pb-[64px] '>
             <div>
                 <Link href='/' className='rounded-[8px] border-[1px] border-[#374af3] border-opacity-20 px-[24px] py-[10px] flex items-center gap-[5px]'>
                     <Image src={backIcon} alt='back icon' />
                     <span>Geri</span>
                 </Link>
             </div>
-            <div className='mt-[60px] w-full'>
-                <span>Daxil ol</span>
-                <div className='mt-[60px] grid grid-cols-2 gap-[40px]'>
+            <div className='mt-[30px] md:mt-[60px] w-full'>
+                <span className='text-[24px] font-semibold text-[#050123]'>Daxil ol</span>
+                <div className='mt-[30px] md:mt-[60px] grid grid-cols-1 md:grid md:grid-cols-2 gap-[30px] md:gap-[40px]'>
                     <input type="text" placeholder='Ad və soyadınız' className='px-[20px] py-[13px] rounded-[12px] border-[1px] border-[#374af3] border-opacity-20 outline-none text-[14px]' />
                     <input type="email" placeholder='E-poçtunuz' className='px-[20px] py-[13px] rounded-[12px] border-[1px] border-[#374af3] border-opacity-20 outline-none text-[14px]' />
                     <input type="tel" placeholder='Telefon nömrəniz' className='px-[20px] py-[13px] rounded-[12px] border-[1px] border-[#374af3] border-opacity-20 outline-none text-[14px]' />
@@ -29,14 +29,14 @@ const page = () => {
                 </div>
                 <Link href='/register/forget-password' className='text-[12px] text-[#374af3]'>Şifrəni unutmusunuz?</Link>
             </div>
-            <div className='w-full flex items-center gap-[40px] mt-[50px]'>
-            <button className='w-1/2 rounded-[12px] bg-[#374af3] py-[16px] text-[16px] font-semibold text-[#fff]'>Qeydiyyat</button>
-                <button className='w-1/2 flex items-center justify-center gap-[10px] py-[16px] bg-[#fff] rounded-[12px] border-[1px] border-[#374af3] border-opacity-20'>
+            <div className='w-full grid grid-cols-1 md:flex md:items-center gap-[30px] md:gap-[40px] mt-[50px]'>
+            <button className='md:w-1/2 rounded-[12px] bg-[#374af3] py-[16px] text-[16px] font-semibold text-[#fff]'>Qeydiyyat</button>
+                <button className='md:w-1/2 flex items-center justify-center gap-[10px] py-[16px] bg-[#fff] rounded-[12px] border-[1px] border-[#374af3] border-opacity-20'>
                     <Image src={googleIcon} alt='google icon' />
                     <span>Google</span>
                 </button>
             </div>
-            <div className='w-full flex items-center justify-center gap-[5px] mt-[60px]'>
+            <div className='w-full flex items-center justify-center gap-[5px] mt-[30px] md:mt-[60px]'>
                 <span className='text-[16px] text-[#050123] '>Hesabınız yoxdur?</span>
                 <Link href='/register' className='text-[16px] text-[#374af3]'>Qeydiyyatdan keçin</Link>
             </div>
